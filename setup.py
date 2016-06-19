172c4beb8147fe77ff02f3390504e2d3339fe55c
Dean Gardiner <me@dgardiner.net>
2016-06-20 00:57:15 +1200

    Updated [setup.py] to use pbr

--- setup.py
+++ setup.py
@@ -2,13 +2,6 @@ from setuptools import setup
 
 
 setup(
-    name='oem-framework',
-    version='1.0.0',
-
-    author="Dean Gardiner",
-    author_email="me@dgardiner.net",
-
-    install_requires=[
-        'bencode>=1.0'
-    ]
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
